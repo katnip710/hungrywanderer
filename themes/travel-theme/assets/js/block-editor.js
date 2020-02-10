d632bf15c9f05386f5c40e0f44d8696c7628467d
Ewan Lan <ewanlan331@hotmail.com>
2020-02-10 18:09:58 -0500

    added foundation, plugins, individual css, assets folder and linked them inside functions.php

---
+++ themes/travel-theme/assets/js/block-editor.js
@@ -0,0 +1,13 @@
+wp.domReady( function() {
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+
+    wp.blocks.registerBlockStyle( 'core/quote', {
+        name: 'fancy-quote',
+        label: 'Fancy Quote',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/heading', {
+        name: 'underline',
+        label: 'underline',
+    } );
+} );
