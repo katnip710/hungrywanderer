8178fe52be8d0976f62d00f2c6d01bc3336cb40b
Ian <ian1498@gmail.com>
2020-02-11 11:08:59 -0500

    Added custom block - border

---
+++ themes/travel-theme/assets/js/custom-blocks.js
@@ -0,0 +1,18 @@
+const { registerBlockType } = wp.blocks;
+
+registerBlockType('wanderers/custom-border', {
+    // built in attributes
+    title: 'Border',
+    description: 'Add border to block',
+    icon: 'format-image',
+    category: 'layout',
+
+    // custom attributes
+    attributes: {},
+
+    // custom functions
+
+    // built-in functions
+    edit() {},
+    save() {}
+}); 
