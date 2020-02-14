9085052be4bd359b401bbb4ee92dc9ed43122106
Ewan Lan <ewanlan331@hotmail.com>
2020-02-14 15:46:19 -0500

    deleted woocommerce-related files

--- themes/travel-theme/functions.php
+++ themes/travel-theme/functions.php
@@ -294,9 +294,4 @@ if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WooCommerce compatibility file.
- */
-if (class_exists('WooCommerce')) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
+
