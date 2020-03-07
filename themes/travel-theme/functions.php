2540099418665b5b79fddef21842f1caf0ae773b
Ian <ian1498@gmail.com>
2020-03-07 11:38:46 -0500

    added footer in display location of Menus

--- themes/travel-theme/functions.php
+++ themes/travel-theme/functions.php
@@ -47,6 +47,7 @@ if (!function_exists('travel_theme_setup')) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(array(
 			'menu-1' => esc_html__('Primary', 'travel-theme'),
+			'footer-menu' => esc_html__( 'Footer', 'travel-theme' ),
 		));
 
 		/*
