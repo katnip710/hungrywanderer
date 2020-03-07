4e5f924a8eecdb439f76219e3ab756d4c162f0c4
Ian <ian1498@gmail.com>
2020-03-07 15:30:27 -0500

    added dynamic social media in footer

--- themes/travel-theme/footer.php
+++ themes/travel-theme/footer.php
@@ -20,11 +20,46 @@
 					$args = array(
 						'theme_location' => 'footer-menu',
 						'after' => ' | ',
-						'container' => '',
+						'container' => 'div',
 					);
 					wp_nav_menu( $args );
 				}
 		?>
+		<!-- // Use the customizer control that has been set -->
+		<?php if ( get_theme_mod('travel_theme_facebook_url') || get_theme_mod('travel_theme_twitter_url') || get_theme_mod('travel_theme_instagram_url') || get_theme_mod('travel_theme_youtube_url') ) { ?>
+			<div class="social-media-section">
+				<ul class="social-media">
+					<?php if ( get_theme_mod( 'travel_theme_facebook_url' ) ) { ?>
+						<li class="facebook">
+							<a href="<?php echo get_theme_mod( 'travel_theme_facebook_url' ); ?>">
+								<i href="#" class="fab fa-facebook-square"></i>
+							</a>
+						</li>
+					<?php } ?>
+					<?php if ( get_theme_mod( 'travel_theme_twitter_url' ) ) { ?>
+						<li class="twitter">
+							<a href="<?php echo get_theme_mod( 'travel_theme_twitter_url' ); ?>">
+								<i href="#" class="fab fa-twitter"></i>
+							</a>
+						</li>
+					<?php } ?>
+					<?php if ( get_theme_mod( 'travel_theme_instagram_url' ) ) { ?>
+						<li class="instagram">
+							<a href="<?php echo get_theme_mod( 'travel_theme_instagram_url' ); ?>">
+							<i href="#" class="fab fa-instagram"></i>
+							</a>
+						</li>
+					<?php } ?>
+					<?php if ( get_theme_mod( 'travel_theme_youtube_url' ) ) { ?>
+						<li class="youtube">
+							<a href="<?php echo get_theme_mod( 'travel_theme_youtube_url' ); ?>">
+								<i href="#" class="fab fa-youtube"></i>
+							</a>
+						</li>
+					<?php } ?>
+				</ul>
+			</div>
+		<?php } ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- themes/travel-theme/inc/customizer.php
+++ themes/travel-theme/inc/customizer.php
@@ -25,7 +25,118 @@ function travel_theme_customize_register( $wp_customize ) {
 			'render_callback' => 'travel_theme_customize_partial_blogdescription',
 		) );
 	}
+
+
+	/**
+	 * PANELS
+	 */
+	// Social Media Panel
+	$wp_customize->add_panel( 'travel_theme_social_media_panel', array(
+		'title' => esc_html__( 'Social Media', 'travel_theme' ),
+		'capability' => 'edit_theme_options',
+	) );
+
+	/**
+	 * SECTIONS
+	 */
+	// Facebook
+	$wp_customize->add_section( 'travel_theme_facebook_section', array(
+		'title' => esc_html__( 'Facebook', 'travel_theme' ),
+		'capability' => 'edit_theme_options',
+		'panel' => 'travel_theme_social_media_panel',
+	) );
+	// Twitter
+	$wp_customize->add_section( 'travel_theme_twitter_section', array(
+		'title' => esc_html__( 'Twitter', 'travel_theme' ),
+		'capability' => 'edit_theme_options',
+		'panel' => 'travel_theme_social_media_panel',
+	) );
+	// Instagram
+	$wp_customize->add_section( 'travel_theme_instagram_section', array(
+		'title' => esc_html__( 'Instagram', 'travel_theme' ),
+		'capability' => 'edit_theme_options',
+		'panel' => 'travel_theme_social_media_panel',
+	) );
+	// YouTube
+	$wp_customize->add_section( 'travel_theme_youtube_section', array(
+		'title' => esc_html__( 'YouTube', 'travel_theme' ),
+		'capability' => 'edit_theme_options',
+		'panel' => 'travel_theme_social_media_panel',
+	) );
+
+	/**
+	 * SETTINGS
+	 */
+	// Facebook
+	$wp_customize->add_setting( 'travel_theme_facebook_url', array(
+		'transport' => 'refresh',
+		'default' => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+	// Twitter
+	$wp_customize->add_setting( 'travel_theme_twitter_url', array(
+		'transport' => 'refresh',
+		'default' => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+	// Instagram
+	$wp_customize->add_setting( 'travel_theme_instagram_url', array(
+		'transport' => 'refresh',
+		'default' => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+	// YouTube
+	$wp_customize->add_setting( 'travel_theme_youtube_url', array(
+		'transport' => 'refresh',
+		'default' => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	/**
+	 * CONTROLS
+	 */
+	// Facebook
+	$wp_customize->add_control( 'travel_theme_facebook_url', array(
+		'label' => esc_html__( 'URL', 'travel_theme' ),
+		'description' => esc_html__( 'Add URL to display Facebook icon/link', 'travel_theme' ),
+		'section' => 'travel_theme_facebook_section',
+		'type' => 'input',
+		'input_attrs' => array(
+			'placeholder' => esc_html__( 'https://facebook.com', 'travel_theme' ),
+		),
+	) );
+	// Twitter
+	$wp_customize->add_control( 'travel_theme_twitter_url', array(
+		'label' => esc_html__( 'URL', 'travel_theme' ),
+		'description' => esc_html__( 'Add URL to display Twitter icon/link', 'travel_theme' ),
+		'section' => 'travel_theme_twitter_section',
+		'type' => 'input',
+		'input_attrs' => array(
+			'placeholder' => esc_html__( 'https://twitter.com', 'travel_theme' ),
+		),
+	) );
+	// Instagram
+	$wp_customize->add_control( 'travel_theme_instagram_url', array(
+		'label' => esc_html__( 'URL', 'travel_theme' ),
+		'description' => esc_html__( 'Add URL to display Instagram icon/link', 'travel_theme' ),
+		'section' => 'travel_theme_instagram_section',
+		'type' => 'input',
+		'input_attrs' => array(
+			'placeholder' => esc_html__( 'https://instagram.com', 'travel_theme' ),
+		),
+	) );
+	// YouTube
+	$wp_customize->add_control( 'travel_theme_youtube_url', array(
+		'label' => esc_html__( 'URL', 'travel_theme' ),
+		'description' => esc_html__( 'Add URL to display YouTube icon/link', 'travel_theme' ),
+		'section' => 'travel_theme_youtube_section',
+		'type' => 'input',
+		'input_attrs' => array(
+			'placeholder' => esc_html__( 'https://youtube.com', 'travel_theme' ),
+		),
+	) );
 }
+
 add_action( 'customize_register', 'travel_theme_customize_register' );
 
 /**
